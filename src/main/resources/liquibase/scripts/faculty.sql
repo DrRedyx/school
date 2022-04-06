beb47c21a085cbb54fa09d0412e9e3c1da5e20d4
DrRedyx <dr.redyx@gmail.com>
2022-04-06 20:37:36 +0300

    Migr

--- src/main/resources/liquibase/scripts/faculty.sql
+++ src/main/resources/liquibase/scripts/faculty.sql
@@ -1,5 +1,5 @@
-- liquibase formatted sql
+-- liquibase formatted sql
 
-- changeset inigga:1
-CREATE INDEX faculty_name_index on faculty (name);
-CREATE INDEX faculty_color_index on faculty (color);
+-- changeset inigga:1
+CREATE INDEX faculty_nc_idx on faculty (name, color);
+
